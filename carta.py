56321812ffc9aeeaa2fb2d9889183fc99322c8fd
M4v3r1ck892 <118367551+M4v3r1ck892@users.noreply.github.com>
2023-06-26 23:04:40 -0600

    cambios  de carpeta

--- carta.py
+++
@@ -1,8 +0,0 @@
-#🎉 OMG we're so freaking proud! (╥﹏╥)#
-print('06/dec/2022')
-print('en este año adquiri un nuevo trabajo como operador de rampa en el aicm de la ciudad de mexico ademas de que')
-print('estoy logrando reaprender el idioma ingles decidi que aun quiero ser programador y que quiero')
-print('aprender lenguajes de programacion')
-print('para mi yo programador futuro de que seas el mejor programador posible y humilde')
-print('suerte en tu camino')
-print('🎉 se el mejor')
